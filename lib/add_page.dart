da3f2dd86b9b8920a49dc37e0570c978167e1500
Aditya <rameshaditya2001@gmail.com>
2023-01-10 00:18:01 +0530

    Added gesture detector, so that I can use the buttons to move back and forth between the routes

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:my_expense/home_page.dart';
 
 class AddExpense extends StatelessWidget {
   const AddExpense({super.key});
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +34,18 @@ class AddExpense extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text("Cancel", style: TextStyle(fontSize: 28)),
-                SizedBox(
-                  width: 10,
-                ),
-                Text("Expense", style: TextStyle(fontSize: 28))
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(_createRoute());
+                    },
+                    child:
+                        const Text("Cancel", style: TextStyle(fontSize: 28))),
+                const Text("Expense", style: TextStyle(fontSize: 28)),
+                const Icon(
+                  Icons.repeat_one_on_rounded,
+                  size: 42,
+                )
               ],
             ),
           )
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -40,106 +40,115 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverList(
-                // better use `SliverChildBuilderDelegate`
-                delegate: SliverChildListDelegate([
-              // you can put regular container here
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Icon(
-                          Icons.search,
-                          size: 32,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                              color: Colors.grey[400], shape: BoxShape.circle),
-                          child: const Icon(
-                            Icons.add,
-                            size: 28,
+        child: RefreshIndicator(
+          onRefresh: handleRefresh,
+          child: CustomScrollView(
+            slivers: [
+              SliverList(
+                  // better use `SliverChildBuilderDelegate`
+                  delegate: SliverChildListDelegate([
+                // you can put regular container here
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.search,
+                            size: 32,
                           ),
+                          GestureDetector(
+                            onTap: handleRefresh,
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[400],
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                size: 28,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 75,
+                    ),
+                    const Text(
+                      "Spent this week",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "₹",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontFeatures: [FontFeature.superscripts()],
+                          ),
+                        ),
+                        Text(
+                          "5,400",
+                          style: TextStyle(fontSize: 75),
+                        ),
+                        Text(
+                          "00",
+                          style: TextStyle(
+                              fontSize: 40,
+                              fontFeatures: [FontFeature.superscripts()]),
                         )
                       ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 75,
-                  ),
-                  const Text(
-                    "Spent this week",
-                    style: TextStyle(fontSize: 32),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "₹",
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontFeatures: [FontFeature.superscripts()],
-                        ),
-                      ),
-                      Text(
-                        "5,400",
-                        style: TextStyle(fontSize: 75),
-                      ),
-                      Text(
-                        "00",
-                        style: TextStyle(
-                            fontSize: 40,
-                            fontFeatures: [FontFeature.superscripts()]),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 75,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Row(
-                      children: const [
-                        SizedBox(
-                          height: 20,
-                          width: 42,
-                        ),
-                        Text(
-                          'Today',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        SizedBox(
-                          height: 20,
-                          width: 160,
-                        ),
-                        Text(
-                          '₹5,400.90',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ],
+                    const SizedBox(
+                      height: 75,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: const [
+                          SizedBox(
+                            height: 20,
+                            width: 42,
+                          ),
+                          Text(
+                            'Today',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          SizedBox(
+                            height: 20,
+                            width: 160,
+                          ),
+                          Text(
+                            '₹5,400.90',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              ])),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return const ExpenseWidget();
+                  },
+                  childCount: 5, // 1000 list items
+                ),
               ),
-            ])),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return const ExpenseWidget();
-                },
-                childCount: 5, // 1000 list items
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
